0a6e16c87757142f1b689a76a54cf345b926df64
Dheshan Mohandass <dheshan.mail@gmail.com>
2020-05-13 18:59:30 +0530

    Configured basic tests for AuthGuard
    Signed-off-by: Dheshan Mohandass <dheshan.mail@gmail.com>

--- src/app/services/auth.guard.spec.ts
+++ src/app/services/auth.guard.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore } from 'angularfire2/firestore';
+
+import { angularFireAuthStub } from '../test/angularfireauth.stub';
+import { angularFirestoreStub } from '../test/angularfirestore.stub';
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {provide: AngularFireAuth, useValue: angularFireAuthStub},
+        {provide: AngularFirestore, useValue: angularFirestoreStub},
+      ],
+    });
     guard = TestBed.inject(AuthGuard);
   });
 
